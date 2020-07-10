a131aedd95ac9e01c2ad7a5cfe0bc4a732cdd837
Alexandre Thenorio <alethenorio@users.noreply.github.com>
2020-07-10 16:43:41 +0200

    Allow for numbers in value descriptors
    They generate valid Go so no need to include them in the camel case

--- internal/identifiers/case.go
+++ internal/identifiers/case.go
@@ -3,10 +3,15 @@ package identifiers
 import "unicode"
 
 func IsCamelCase(s string) bool {
-	for i, r := range s {
+	i := 0
+	for _, r := range s {
+		if unicode.IsDigit(r) {
+			continue
+		}
 		if i == 0 && !unicode.IsUpper(r) || !IsAlphaChar(r) && !IsNumChar(r) {
 			return false
 		}
+		i++
 	}
 	return true
 }
