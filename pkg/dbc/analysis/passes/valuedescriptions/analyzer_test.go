7c556f419d443d906264c57a630e067be5dd5bb0
Alexandre Thenorio <alethenorio@users.noreply.github.com>
2020-07-10 16:44:36 +0200

    CamelCase value descriptor lint error marker the description
    It was pointing to the value instead

--- pkg/dbc/analysis/passes/valuedescriptions/analyzer_test.go
+++ pkg/dbc/analysis/passes/valuedescriptions/analyzer_test.go
@@ -14,14 +14,27 @@ func TestAnalyzer(t *testing.T) {
 			Name: "ok",
 			Data: `VAL_ 100 Command 2 "Reboot" 1 "Sync" 0 "Noop";`,
 		},
-
+		{
+			Name: "ok",
+			Data: `VAL_ 100 Command 2 "11Reboot" 1 "123" 0 "Noop";`,
+		},
 		{
 			Name: "underscore",
 			Data: `VAL_ 100 Command 2 "Reboot_Command" 1 "Sync" 0 "Noop";`,
 			Diagnostics: []*analysis.Diagnostic{
 				{
-					Pos:     scanner.Position{Line: 1, Column: 18},
-					Message: "value description must be CamelCase",
+					Pos:     scanner.Position{Line: 1, Column: 21},
+					Message: "value description must be CamelCase (numbers ignored)",
+				},
+			},
+		},
+		{
+			Name: "several digits value",
+			Data: `VAL_ 100 Command 234 "Reboot_Command" 1 "Sync" 0 "Noop";`,
+			Diagnostics: []*analysis.Diagnostic{
+				{
+					Pos:     scanner.Position{Line: 1, Column: 23},
+					Message: "value description must be CamelCase (numbers ignored)",
 				},
 			},
 		},
