d5c93bd340dfa03b5081abf46e56351c319b72ce
Alexandre Thenorio <alethenorio@users.noreply.github.com>
2020-07-10 16:43:41 +0200

    Allow for numbers in value descriptors
    They generate valid Go so no need to include them in the camel case

--- internal/identifiers/case_test.go
+++ internal/identifiers/case_test.go
@@ -10,7 +10,9 @@ func TestIsCamelCase(t *testing.T) {
 	assert.Assert(t, IsCamelCase("SOC"))
 	assert.Assert(t, IsCamelCase("Camel"))
 	assert.Assert(t, IsCamelCase("CamelCase"))
+	assert.Assert(t, IsCamelCase("111CamelCaseNr"))
 	assert.Assert(t, !IsCamelCase("camelCase"))
 	assert.Assert(t, !IsCamelCase("snake_case"))
 	assert.Assert(t, !IsCamelCase("kebab-case"))
+	assert.Assert(t, !IsCamelCase("111camelCaseNr"))
 }
