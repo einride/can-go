15dddf5ace78d61052f853a007f28faa3431358d
Oscar Söderlund <odsod@users.noreply.github.com>
2020-11-10 11:24:52 +0100

    chore(deps): bump golangci-lint
    Uses prebuilt binary for faster build.

--- tools/golangci-lint/go.mod
+++
@@ -1,5 +0,0 @@
-module tools/golangci-lint
-
-go 1.13
-
-require github.com/golangci/golangci-lint v1.27.0
--- tools/golangci-lint/rules.mk
+++ tools/golangci-lint/rules.mk
@@ -1,13 +1,24 @@
 golangci_lint_cwd := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
-golangci_lint := $(golangci_lint_cwd)/bin/golangci-lint
+golangci_lint_version := 1.30.0
+golangci_lint := $(golangci_lint_cwd)/$(golangci_lint_version)/golangci-lint
 
-$(golangci_lint): $(golangci_lint_cwd)/go.mod
-	@echo building golangci-lint...
-	@cd $(golangci_lint_cwd) && go build -o $@ github.com/golangci/golangci-lint/cmd/golangci-lint
-	@cd $(golangci_lint_cwd) && go mod tidy
+ifeq ($(shell uname),Linux)
+golangci_lint_archive_url := https://github.com/golangci/golangci-lint/releases/download/v${golangci_lint_version}/golangci-lint-${golangci_lint_version}-linux-amd64.tar.gz
+else ifeq ($(shell uname),Darwin)
+golangci_lint_archive_url := https://github.com/golangci/golangci-lint/releases/download/v${golangci_lint_version}/golangci-lint-${golangci_lint_version}-darwin-amd64.tar.gz
+else
+$(error unsupported OS: $(shell uname))
+endif
 
-# go-lint: lint Go code with GolangCI-Lint
+$(golangci_lint):
+	$(info building golangci-lint...)
+	@mkdir -p $(dir $@)
+	@curl -sSL $(golangci_lint_archive_url) -o - | \
+		tar -xz --directory $(dir $@) --strip-components 1
+	@chmod +x $@
+	@touch $@
+
 .PHONY: go-lint
 go-lint: $(golangci_lint)
-	@echo linting Go code with golangci-lint...
+	$(info linting Go code with golangci-lint...)
 	@$(golangci_lint) run
