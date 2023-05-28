cee39d87b98bf68d58b32f83b6ef4f810797d1b1
anajavi <anajavi@users.noreply.github.com>
2023-05-28 23:02:15 +0300

    feat: add socketcan option to receive error frames

--- pkg/socketcan/dialraw_linux.go
+++ pkg/socketcan/dialraw_linux.go
@@ -10,12 +10,20 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-func dialRaw(device string) (conn net.Conn, err error) {
+type dialOpts struct {
+	errorFrameMask *int
+}
+
+func dialRaw(device string, opt ...DialOption) (conn net.Conn, err error) {
 	defer func() {
 		if err != nil {
 			err = &net.OpError{Op: "dial", Net: canRawNetwork, Addr: &canRawAddr{device: device}, Err: err}
 		}
 	}()
+	opts := dialOpts{}
+	for _, f := range opt {
+		f(&opts)
+	}
 	ifi, err := net.InterfaceByName(device)
 	if err != nil {
 		return nil, fmt.Errorf("interface %s: %w", device, err)
@@ -23,6 +31,11 @@ func dialRaw(device string) (conn net.Conn, err error) {
 	fd, err := unix.Socket(unix.AF_CAN, unix.SOCK_RAW, unix.CAN_RAW)
 	if err != nil {
 		return nil, fmt.Errorf("socket: %w", err)
+	}
+	if opts.errorFrameMask != nil {
+		if err := unix.SetsockoptInt(fd, unix.SOL_CAN_RAW, unix.CAN_RAW_ERR_FILTER, *opts.errorFrameMask); err != nil {
+			return nil, fmt.Errorf("set error filter: %w", err)
+		}
 	}
 	// put fd in non-blocking mode so the created file will be registered by the runtime poller (Go >= 1.12)
 	if err := unix.SetNonblock(fd, true); err != nil {
@@ -32,4 +45,13 @@ func dialRaw(device string) (conn net.Conn, err error) {
 		return nil, fmt.Errorf("bind: %w", err)
 	}
 	return &fileConn{ra: &canRawAddr{device: device}, f: os.NewFile(uintptr(fd), "can")}, nil
+}
+
+// WithReceiveErrorFrames returns a DialOption which enables
+// can error frame receiving on can port.
+func WithReceiveErrorFrames() DialOption {
+	return func(o *dialOpts) {
+		canErrMask := unix.CAN_ERR_MASK
+		o.errorFrameMask = &canErrMask
+	}
 }
